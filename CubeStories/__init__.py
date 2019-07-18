63a0e1b01d246de5e6a56b6064a7b6880d58c6f3
MaciejJanowski <maciej.janowski@insight-centre.org>
2019-07-18 22:03:43 +0100

    All analytical Patterns encapsulated in objs

--- CubeStories/__init__.py
+++ CubeStories/__init__.py
@@ -1,3 +1,3 @@
 from .metaData.metaData import metaData
 from .cubeParameters.cubeParameters import cubeParameters
-from .storyPattern import measCount,leagueTable,dissFactors,profOutliers,analysisByCategory
+from .storyPattern import *
