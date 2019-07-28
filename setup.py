83afc25caf08e9111e03b73027a28a8ee8883c50
MaciejJanowski <maciej.janowski@insight-centre.org>
2019-07-28 22:12:19 +0100

    0.3.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='cubestories',
-      version='0.3.8',
+      version='0.3.9',
       url='https://github.com/MaciejJanowski/CubeStories',
       license='MIT',
       author='Maciej Janowski',
